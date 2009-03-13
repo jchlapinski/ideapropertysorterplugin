c7190ecfb91ac99f5be8422c5895d33e738c3b3d
oliver.wehrens <oliver.wehrens@87bf0912-024d-11de-802b-177316e4ac06>
2009-03-13 22:09:12 +0000

    First version, initial import

---
+++ trunk/src/net/wehrens/intellij/plugins/propertysorter/ConvertException.java
@@ -0,0 +1,11 @@
+package net.wehrens.intellij.plugins.propertysorter;
+
+public class ConvertException extends RuntimeException {
+
+    public ConvertException(String s) {
+        super(s);
+    }
+
+
+}
+
